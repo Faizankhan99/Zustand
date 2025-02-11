458fd1383befdb109442573fa4b95c6be5b086c3
monika <veerati.monika@gmail.com>
2025-02-11 16:47:19 -0500

    fifth

--- File1.tsx
+++ File1.tsx
@@ -1,9 +1,6 @@
 Monika
 Faizan
-Arvind
-
-mkdir git-rebase-conflict
-cd git-rebase-conflict
-git init
-git add file.txt
-git commit -m "Initial commit on main"
+<Arvind2></Arvind2>
+git
+monika
+moniak
